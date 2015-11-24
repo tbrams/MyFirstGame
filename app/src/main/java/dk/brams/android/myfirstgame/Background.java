572f6727786f355ea9a1e1ec7a0f584258833372
tbrams <torben@brams.dk>
2015-11-24 13:48:18 +0100

    Moving background

--- app/src/main/java/dk/brams/android/myfirstgame/Background.java
+++ app/src/main/java/dk/brams/android/myfirstgame/Background.java
@@ -1,7 +1,38 @@
 package dk.brams.android.myfirstgame;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
 /**
  * Created by tbrams on 24/11/15.
  */
 public class Background {
+    private Bitmap image;
+    private int x, y, dx;
+
+    public Background(Bitmap res) {
+        image = res;
+    }
+
+
+    public void update() {
+        x+=dx;
+        if(x<-GamePanel.WIDTH){
+            x=0;
+        }
+    }
+
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, x, y,null);
+        if(x<0)
+        {
+            canvas.drawBitmap(image, x+GamePanel.WIDTH, y, null);
+        }
+    }
+
+
+    public void setVector(int dx) {
+        this.dx = dx;
+    }
 }
--- app/src/main/java/dk/brams/android/myfirstgame/GamePanel.java
+++ app/src/main/java/dk/brams/android/myfirstgame/GamePanel.java
@@ -1,6 +1,8 @@
 package dk.brams.android.myfirstgame;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -9,8 +11,13 @@ import android.view.SurfaceView;
  * Created by tbrams on 24/11/15.
  */
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+    public static final int WIDTH = 856;
+    public static final int HEIGHT = 480;
     private MainThread thread;
-    public GamePanel(Context context) {
+    private Background bg;
+
+    public GamePanel(Context context)
+    {
         super(context);
 
 
@@ -29,7 +36,8 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(SurfaceHolder holder){
         boolean retry = true;
-        while(retry) {
+        while(retry)
+        {
             try{thread.setRunning(false);
                 thread.join();
 
@@ -42,18 +50,34 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceCreated(SurfaceHolder holder){
 
+        bg = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.grassbg1));
+        bg.setVector(-5);
         //we can safely start the game loop
         thread.setRunning(true);
         thread.start();
 
     }
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    public boolean onTouchEvent(MotionEvent event)
+    {
         return super.onTouchEvent(event);
     }
 
+    public void update()
+    {
 
-    public void update() {
-
+        bg.update();
+    }
+    @Override
+    public void draw(Canvas canvas)
+    {
+        final float scaleFactorX = getWidth()/WIDTH;
+        final float scaleFactorY = getHeight()/HEIGHT;
+        if(canvas!=null) {
+            final int savedState = canvas.save();
+            canvas.scale(scaleFactorX, scaleFactorY);
+            bg.draw(canvas);
+            canvas.restoreToCount(savedState);
+        }
     }
 }
