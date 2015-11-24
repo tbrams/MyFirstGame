72622ab2ddb8a55d325003de3f04149c3d65adc8
tbrams <torben@brams.dk>
2015-11-24 13:40:10 +0100

    First commit

---
+++ app/src/main/java/dk/brams/android/myfirstgame/Background.java
@@ -0,0 +1,7 @@
+package dk.brams.android.myfirstgame;
+
+/**
+ * Created by tbrams on 24/11/15.
+ */
+public class Background {
+}
---
+++ app/src/main/java/dk/brams/android/myfirstgame/Game.java
@@ -0,0 +1,23 @@
+package dk.brams.android.myfirstgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Game extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //set to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new GamePanel(this));
+    }
+
+}
