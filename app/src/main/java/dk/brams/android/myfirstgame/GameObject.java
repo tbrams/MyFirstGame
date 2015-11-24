1b0a3d25ab8f2f70b1a20856d7bba88906797435
tbrams <torben@brams.dk>
2015-11-24 14:03:57 +0100

    Not really working.... but here it is

---
+++ app/src/main/java/dk/brams/android/myfirstgame/GameObject.java
@@ -0,0 +1,44 @@
+package dk.brams.android.myfirstgame;
+
+import android.graphics.Rect;
+
+/**
+ * Created by tbrams on 24/11/15.
+ */
+public class GameObject {
+    protected int x;
+    protected int y;
+    protected int dy;
+    protected int dx;
+    protected int width;
+    protected int height;
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public int getWidth()
+    {
+        return width;
+    }
+    public Rect getRectangle()
+    {
+        return new Rect(x, y, x+width, y+height);
+    }
+}
--- app/src/main/java/dk/brams/android/myfirstgame/GamePanel.java
+++ app/src/main/java/dk/brams/android/myfirstgame/GamePanel.java
@@ -13,11 +13,12 @@ import android.view.SurfaceView;
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     public static final int WIDTH = 856;
     public static final int HEIGHT = 480;
+    public static final int MOVESPEED = -5;
     private MainThread thread;
     private Background bg;
+    private Player player;
 
-    public GamePanel(Context context)
-    {
+    public GamePanel(Context context) {
         super(context);
 
 
@@ -36,8 +37,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(SurfaceHolder holder){
         boolean retry = true;
-        while(retry)
-        {
+        while(retry) {
             try{thread.setRunning(false);
                 thread.join();
 
@@ -51,33 +51,56 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     public void surfaceCreated(SurfaceHolder holder){
 
         bg = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.grassbg1));
-        bg.setVector(-5);
+        player = new Player(BitmapFactory.decodeResource(getResources(), R.drawable.helicopter), 65, 25, 3);
         //we can safely start the game loop
         thread.setRunning(true);
         thread.start();
 
     }
+
+
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction()==MotionEvent.ACTION_DOWN){
+            if(!player.getPlaying()) {
+                player.setPlaying(true);
+            } else {
+                player.setUp(true);
+            }
+            return true;
+        }
+
+        if(event.getAction()==MotionEvent.ACTION_UP) {
+            player.setUp(false);
+            return true;
+        }
+
         return super.onTouchEvent(event);
     }
 
-    public void update()
-    {
 
-        bg.update();
+
+    public void update() {
+        if(player.getPlaying()) {
+            bg.update();
+            player.update();
+        }
     }
+
+
     @Override
-    public void draw(Canvas canvas)
-    {
-        final float scaleFactorX = getWidth()/WIDTH;
-        final float scaleFactorY = getHeight()/HEIGHT;
+    public void draw(Canvas canvas) {
+        final float scaleFactorX = getWidth()/(WIDTH*1.f);
+        final float scaleFactorY = getHeight()/(HEIGHT*1.f);
+
         if(canvas!=null) {
             final int savedState = canvas.save();
             canvas.scale(scaleFactorX, scaleFactorY);
             bg.draw(canvas);
+            player.draw(canvas);
             canvas.restoreToCount(savedState);
         }
     }
+
+
 }
